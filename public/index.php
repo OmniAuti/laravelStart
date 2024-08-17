87bfa9e9abb3e41fdb79a3f433c55897bfa1e47f
OmniOmni <austin.olijar@gmail.com>
2024-08-17 16:22:07 -0500

    linked

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-echo('start');
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
@@ -13,5 +13,4 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+(require_once __DIR__.'/../bootstrap/app.php')->handleRequest(Request::capture());
